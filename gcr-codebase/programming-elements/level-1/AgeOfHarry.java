f387c0d7cca0c4a794c2fae403468774cc5caaad
Prathamkacher <pratham9340@gmail.com>
2025-12-18 12:35:45 +0530

    [Pratham] : added find age of harry program

---
+++ gcr-codebase/programming-elements/level-1/AgeOfHarry.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class AgeOfHarry{
+	public static void main(String agrs[]){
+		//Multiline Variable for birthYear and currentYear
+		int birthYear = 2000, currentYear = 2024;
+		
+		//Singleline Variable for ageOfHarry
+		int ageOfHarry = currentYear - birthYear;
+		
+		//Display the current age of Haary in 2024
+		System.out.println("Harry' age in 2024 is " + (ageOfHarry));
+	}
+}
