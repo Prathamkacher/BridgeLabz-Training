b8fbf5b9e98be1a07bf350d49817fbf3821250a5
Prathamkacher <pratham9340@gmail.com>
2025-12-28 23:09:31 +0530

    [Pratham] : added Hotel Booking program

---
+++ java-oops-practice/gcr-codebase/javaOopsConcept/src/com/constructorandaccessmodifiers/level1/HotelBooking.java
@@ -0,0 +1,61 @@
+package com.constructorandaccessmodifiers.level1;
+
+public class HotelBooking {
+	
+	// Attributes
+    String guestName;
+    String roomType;
+    int nights;
+
+    // Default constructor
+    public HotelBooking() {
+        guestName = "Guest";
+        roomType = "Standard";
+        nights = 1;
+    }
+
+    // Parameterized constructor
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    // Copy constructor
+    public HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+
+    // Method to display booking details
+    public void displayBooking() {
+        System.out.println("Guest Name : " + guestName);
+        System.out.println("Room Type  : " + roomType);
+        System.out.println("Nights     : " + nights);
+    }
+
+    // Main method for testing
+	public static void main(String[] args) {
+		// Using default constructor
+        HotelBooking booking1 = new HotelBooking();
+        System.out.println("Default Booking:");
+        booking1.displayBooking();
+
+        System.out.println();
+
+        // Using parameterized constructor
+        HotelBooking booking2 =
+                new HotelBooking("Rahul Sharma", "Deluxe", 3);
+        System.out.println("Parameterized Booking:");
+        booking2.displayBooking();
+
+        System.out.println();
+
+        // Using copy constructor
+        HotelBooking booking3 = new HotelBooking(booking2);
+        System.out.println("Copied Booking:");
+        booking3.displayBooking();
+	}
+
+}
