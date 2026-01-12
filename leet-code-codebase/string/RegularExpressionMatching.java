70178c18a7e79c6e376af43f4030f33dc78096c3
Prathamkacher <pratham9340@gmail.com>
2026-01-12 23:49:18 +0530

    [Pratham] : added  Regular Expression Matching program

---
+++ leet-code-codebase/string/RegularExpressionMatching.java
@@ -0,0 +1,24 @@
+/*
+10-Regular Expression Matching
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+The matching should cover the entire input string (not partial).
+*/
+
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (p.isEmpty()) return s.isEmpty();
+
+        boolean firstMatch = (!s.isEmpty() &&
+                (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.'));
+
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            return isMatch(s, p.substring(2)) ||
+                   (firstMatch && isMatch(s.substring(1), p));
+        } 
+        else {
+            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+}
