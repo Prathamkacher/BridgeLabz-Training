a0bb7ab055e7a0ad957f6b701832844258435118
Prathamkacher <pratham9340@gmail.com>
2026-01-24 16:07:25 +0530

    [Pratham] : added Multiple Catch Blocks – Handling ArrayIndexOutOfBoundsException and NullPointerException program

---
+++ java-collection-practice/gcr-codebase/JavaCollectionConcepts/src/com/exceptions/multiplecatchexception/MultipleCatch.java
@@ -0,0 +1,29 @@
+package com.exceptions.multiplecatchexception;
+
+import java.util.Scanner;
+
+public class MultipleCatch{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // change this to null to test NullPointerException
+        int[] arr = {10, 20, 30, 40, 50};
+
+        try {
+            System.out.print("Enter index: ");
+            int index = sc.nextInt();
+
+            System.out.println("Value at index " + index + ": " + arr[index]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+        }
+        catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+
+        sc.close();
+    }
+}
