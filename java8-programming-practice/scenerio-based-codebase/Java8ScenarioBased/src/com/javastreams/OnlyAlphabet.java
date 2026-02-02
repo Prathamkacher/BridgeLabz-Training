26717fdb1872a9a149f48011c20da59a571a965c
Prathamkacher <pratham9340@gmail.com>
2026-02-02 16:04:50 +0530

    [Pratham]: added Only Alphabet Characters program

---
+++ java8-programming-practice/scenerio-based-codebase/Java8ScenarioBased/src/com/javastreams/OnlyAlphabet.java
@@ -0,0 +1,18 @@
+package com.javastreams;
+
+import java.util.stream.Collectors;
+
+public class OnlyAlphabet {
+	public void main(String[] args) {
+		String input = "java8stream2025";
+		
+		String result = 
+				input.chars()
+				.filter(Character::isLetter)
+				.mapToObj(c -> String.valueOf((char) c))
+				.collect(Collectors.joining());
+		
+		System.out.println(result);
+	}
+
+}
