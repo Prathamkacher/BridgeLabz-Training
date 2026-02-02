874a294ed368c79299e7db8f376a1316dc443eb9
Prathamkacher <pratham9340@gmail.com>
2026-02-02 15:17:57 +0530

    [Pratham]: added First Non-Repeating Character program

---
+++ java8-programming-practice/scenerio-based-codebase/Java8ScenarioBased/src/com/javastreams/FirstNonRepeatingChar.java
@@ -0,0 +1,28 @@
+package com.javastreams;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeatingChar {
+	public static void main(String[] args) {
+		String input = "stress";
+		
+		Character firstChar =
+				input.chars()
+				.mapToObj(c -> (char) c)
+				.collect(Collectors.groupingBy(
+						c -> c, 
+						LinkedHashMap::new,
+						Collectors.counting()
+				))
+				.entrySet()
+				.stream()
+				.filter(e -> e.getValue() == 1)
+				.map(Map.Entry::getKey)
+				.findFirst()
+				.orElse(null);
+		
+		System.out.println(firstChar);
+	}
+}
