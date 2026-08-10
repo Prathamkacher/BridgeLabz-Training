ea4c5e16bab1488cea88a9f17c15078ded20ab49
Prathamkacher <pratham9340@gmail.com>
2026-08-10 15:54:01 +0530

    [Pratham]: added Davis' Staircase (Recursion problem)

---
+++ Refresher-Training/Day-7/HackerRank-based-codebase/StepPerms.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'stepPerms' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+    public static int[] dp;
+    
+    public static int stepPerms(int n) {
+    // Write your code here
+        if(n < 0){
+            return 0;
+        }
+        if(n == 0){
+            return 1;
+        }
+        
+        if (dp == null || dp.length <= n) {
+            dp = new int[n + 1];
+        }
+        if (dp[n] != 0){
+            return dp[n];
+        }
+        
+        dp[n] = stepPerms(n-1) + stepPerms(n-2) + stepPerms(n-3);
+        
+        return dp[n];
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int s = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, s).forEach(sItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int res = Result.stepPerms(n);
+
+                bufferedWriter.write(String.valueOf(res));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
