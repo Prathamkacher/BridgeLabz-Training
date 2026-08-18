08a46b0420eb2feeca9640e54e477a13a9e88815
Prathamkacher <pratham9340@gmail.com>
2026-08-18 15:47:14 +0530

    [Pratham]: added Static Method Hiding Demo (OOPs Problem)

---
+++ Refresher-Training/Day-13/HackerRank-based-codebase/HidingDemo/Parent.java
@@ -0,0 +1,13 @@
+package HidingDemo;
+
+public class Parent {
+    static void show() {
+        System.out.println("Parent static method");
+    }
+}
+
+class Child extends Parent {
+    static void show() {
+        System.out.println("Child static method");
+    }
+}
