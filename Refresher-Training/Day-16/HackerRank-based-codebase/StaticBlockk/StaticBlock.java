9bea4f371354ca1fcf2071ea96cf640d4da845e3
Prathamkacher <pratham9340@gmail.com>
2026-08-23 17:19:47 +0530

    [Pratham]: added Static Block (OOPs Problem)

---
+++ Refresher-Training/Day-16/HackerRank-based-codebase/StaticBlockk/StaticBlock.java
@@ -0,0 +1,27 @@
+package StaticBlockk;
+
+import java.util.Scanner;
+
+public class StaticBlock {
+
+    static int B;
+    static int H;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+
+        B = sc.nextInt();
+        H = sc.nextInt();
+
+        if (B <= 0 || H <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        if (B > 0 && H > 0) {
+            System.out.println(B * H);
+        }
+    }
+}
